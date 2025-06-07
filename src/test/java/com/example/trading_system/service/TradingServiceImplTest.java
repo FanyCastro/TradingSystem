50b8f9dbc2cfcb660ec9ac69d8510c158b939d72
FanyCastro <estefania.castro.vizoso@gmail.com>
2025-06-07 16:09:59 +0200

    fix: TradingServiceImplTest

--- src/test/java/com/example/trading_system/service/TradingServiceImplTest.java
+++ src/test/java/com/example/trading_system/service/TradingServiceImplTest.java
@@ -5,6 +5,7 @@ import com.example.trading_system.model.Instrument;
 import com.example.trading_system.model.Order;
 import com.example.trading_system.model.Trade;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -18,24 +19,28 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * Unit tests for TradingService logic.
  */
 class TradingServiceImplTest {
-    private TradingService tradingService;
+    private TradingService tradingService = new TradingServiceImpl();
+    private static Instrument instrument;
 
-    Instrument instrument = new Instrument("AAPL");
+    @BeforeAll
+    static void setUpAll() {
+        instrument = new Instrument("AAPL");
+    }
 
     @BeforeEach
     void setUp() {
         tradingService = new TradingServiceImpl();
-
-        tradingService.registerInstrument(instrument);
     }
 
     @Test
     void testRegisterInstrument() {
+        tradingService.registerInstrument(instrument);
         assertTrue(tradingService.getAllInstruments().contains(instrument));
     }
 
     @Test
     void testPlaceOrderAndMatch() {
+        tradingService.registerInstrument(instrument);
         Order buyOrder = new Order(instrument.getId(), "trader1", Order.OrderType.BUY, new BigDecimal("110"), 10);
         Order sellOrder = new Order(instrument.getId(), "trader2", Order.OrderType.SELL, new BigDecimal("100"), 10);
         
@@ -49,6 +54,7 @@ class TradingServiceImplTest {
 
     @Test
     void testCancelOrder() {
+        tradingService.registerInstrument(instrument);
         Order buyOrder = new Order(instrument.getId(), "trader1", Order.OrderType.BUY, new BigDecimal("110"), 10);
 
         tradingService.placeOrder(buyOrder);
@@ -59,6 +65,7 @@ class TradingServiceImplTest {
 
     @Test
     void testGetMarketPrice_shouldReturnZeroAfterMatching() {
+        tradingService.registerInstrument(instrument);
         // After matching - should return zero as all orders are filled
         Order sellOrder = new Order(instrument.getId(), "trader2", Order.OrderType.SELL, new BigDecimal("50"), 4);
         tradingService.placeOrder(sellOrder);
@@ -69,6 +76,7 @@ class TradingServiceImplTest {
 
     @Test
     void testGetMarketPrice_shouldReturnZeroWhenNoOrders() {
+        tradingService.registerInstrument(instrument);
         // No orders - should return zero
         // TODO - review this test
         assertEquals(BigDecimal.ZERO, tradingService.getMarketPrice(instrument.getId()));
@@ -76,6 +84,7 @@ class TradingServiceImplTest {
 
     @Test
     void testGetMarketPrice_shouldReturnZeroWithOnlySellOrder() {
+        tradingService.registerInstrument(instrument);
         // Only buy order - should return 0 (as we don't have best sell price)
         Order sellOrder = new Order(instrument.getId(), "trader2", Order.OrderType.SELL, new BigDecimal("100"), 10);
         tradingService.placeOrder(sellOrder);
@@ -84,6 +93,7 @@ class TradingServiceImplTest {
 
     @Test
     void testGetMarketPrice_ShouldReturnZeroWithOnlyBuyOrder() {
+        tradingService.registerInstrument(instrument);
         // Only buy order - should return 0 (as we don't have best sell price)
         Order buyOrder = new Order(instrument.getId(), "trader1", Order.OrderType.BUY, new BigDecimal("110"), 10);
         tradingService.placeOrder(buyOrder);
@@ -92,6 +102,7 @@ class TradingServiceImplTest {
 
     @Test
     void testGetMarketPrice_shouldReturnCalcWithSellAndBuyOrder() {
+        tradingService.registerInstrument(instrument);
         // Both buy and sell orders - should return average
         Order sellOrder = new Order(instrument.getId(), "trader2", Order.OrderType.SELL, new BigDecimal("100"), 2);
         tradingService.placeOrder(sellOrder);
@@ -113,7 +124,7 @@ class TradingServiceImplTest {
         tradingService.registerInstrument(instrument3);
         
         Collection<Instrument> instruments = tradingService.getAllInstruments();
-        assertEquals(4, instruments.size());
+        assertEquals(3, instruments.size());
         assertTrue(instruments.contains(instrument1));
         assertTrue(instruments.contains(instrument2));
         assertTrue(instruments.contains(instrument3));
